6cf6c1ded6500c530356ff7fbca9e70695003b88
Sulthan A Zhafran <sulthanarifzhafran3@gmail.com>
2024-08-14 08:08:24 +0700

    route update

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('kknjkp-edukasistunting.info/', function() {
+    return view('welcome');
+});
